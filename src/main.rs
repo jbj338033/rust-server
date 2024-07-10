9d2699d694aec0dd2b788b4ca2597d3aa84406f0
전민오 <jbj338033@gmail.com>
2024-07-10 10:26:38 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
@@ -6,6 +9,25 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("src/index.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
